0a3e17a1eb31b689e3cd343d74dd62d024ca8bfb
jeypcabase <jp.vincent.cabase@etousoft.com>
2022-03-18 14:59:29 +0800

    initial commit, laravel + angular with JWT authentication

---
+++ backend/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+
+Route::group([
+
+    'middleware' => 'api',
+
+], function ($router) {
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('signup', 'App\Http\Controllers\AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
